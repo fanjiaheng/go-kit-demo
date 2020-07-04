ab689f407821b60b7dfef4b9a7b6b887ece2948c
felix <779358334@qq.com>
2020-07-04 16:02:26 +0800

    go-kit微服务：日志功能

---
+++ 02-log-demo/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-kit/kit/log"
+
+	kit_svc "01-resful-demo/service"
+)
+
+func main() {
+
+	ctx := context.Background()
+	errChan := make(chan error)
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	var svc kit_svc.Service
+	svc = kit_svc.ArithmeticService{}
+
+	// 装饰器模式，添加日志中间件
+	svc = kit_svc.LoggingMiddleware(logger)(svc)
+
+	endpoint := kit_svc.MakeArithmeticEndpoint(svc)
+
+	r := kit_svc.MakeHttpHandler(ctx, endpoint, logger)
+
+	go func() {
+		fmt.Println("Http Server start at port:9000")
+		handler := r
+		errChan <- http.ListenAndServe(":9000", handler)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Println(<-errChan)
+}
