8b84a5fb753677c1763cab16cb0838f999948906
felix <779358334@qq.com>
2020-07-04 22:01:33 +0800

    go-kit微服务：一个简单的API网关

---
+++ 06-gateway-demo/register/service/transports.go
@@ -0,0 +1,87 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/go-kit/kit/log"
+	kithttp "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	ErrorBadRequest = errors.New("invalid request parameter")
+)
+
+// MakeHttpHandler make http handler use mux
+func MakeHttpHandler(ctx context.Context, endpoints ArithmeticEndpoints, logger log.Logger) http.Handler {
+	r := mux.NewRouter()
+
+	options := []kithttp.ServerOption{
+		kithttp.ServerErrorLogger(logger),
+		kithttp.ServerErrorEncoder(kithttp.DefaultErrorEncoder),
+	}
+
+	r.Methods("POST").Path("/calculate/{type}/{a}/{b}").Handler(kithttp.NewServer(
+		endpoints.ArithmeticEndpoint,
+		decodeArithmeticRequest,
+		encodeArithmeticResponse,
+		options...,
+	))
+
+	// 新增用于Prometheus轮循拉取监控指标的代码，开放API接口/metrics
+	r.Path("/metrics").Handler(promhttp.Handler())
+
+	// 新增用于服务注册与发现的代码，开放API接口/health
+	r.Methods("GET").Path("/health").Handler(kithttp.NewServer(
+		endpoints.HealthCheckEndpoint,
+		decodeHealthCheckRequest,
+		encodeArithmeticResponse,
+		options...,
+	))
+
+	return r
+}
+
+// decodeArithmeticRequest decode request params to struct
+func decodeArithmeticRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	vars := mux.Vars(r)
+	requestType, ok := vars["type"]
+	if !ok {
+		return nil, ErrorBadRequest
+	}
+
+	pa, ok := vars["a"]
+	if !ok {
+		return nil, ErrorBadRequest
+	}
+
+	pb, ok := vars["b"]
+	if !ok {
+		return nil, ErrorBadRequest
+	}
+
+	a, _ := strconv.Atoi(pa)
+	b, _ := strconv.Atoi(pb)
+
+	return ArithmeticRequest{
+		RequestType: requestType,
+		A:           a,
+		B:           b,
+	}, nil
+}
+
+// encodeArithmeticResponse encode response to return
+func encodeArithmeticResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	return json.NewEncoder(w).Encode(response)
+}
+
+// decodeHealthCheckRequest decode request
+func decodeHealthCheckRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	return HealthRequest{}, nil
+}
